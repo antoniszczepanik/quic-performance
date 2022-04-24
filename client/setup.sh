698578a48e1d492ef527281c08ae4b234673ac60
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2022-04-24 21:18:45 +0200

    Increase udp recieve buffer on both hosts

--- client/setup.sh
+++ client/setup.sh
@@ -4,4 +4,8 @@ echo "set -o vi" > ~/.profile # First things first.
 ./install-nix.sh
 source ~/.profile # Make sure nix is in the path.
 
+# Increase UDP recieve  buffer sizes.
+sudo sysctl -w net.core.rmem_max=21299200
+sudo sysctl -w net.core.rmem_default=21299200
+
 screen -dm bash -c "source ~/.profile; /home/ubuntu/run_notebook.sh; exec sh"
--- nginx/setup.sh
+++ nginx/setup.sh
@@ -2,11 +2,16 @@
 
 # A wrapper script for setting up nginx server.
 
-echo "set -o vi" > ~/.profile # First things first.
+echo "set -o vi" >> ~/.profile # First things first.
 ./install-nix.sh
 mkdir /home/ubuntu/data
 echo "<p> Hello, World! </p>" > /home/ubuntu/data/index.html
 head -c 2G /dev/urandom > /home/ubuntu/data/2g.txt
 source ~/.profile # Make sure nix is in the path.
+
+# Increase UDP recieve  buffer sizes.
+sudo sysctl -w net.core.rmem_max=21299200
+sudo sysctl -w net.core.rmem_default=21299200
+
 ./install-nginx.sh
 screen -dm bash -c "source ~/.profile; /home/ubuntu/run-iperf.sh; exec sh"
