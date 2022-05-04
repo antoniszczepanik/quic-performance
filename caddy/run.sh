b4a5ca5cc7ec6a6a823b988a881d7b905752032b
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2022-05-04 19:31:15 +0200

    Fix caddy run.sh script

--- caddy/run.sh
+++ caddy/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-install -y install certbot
+sudo apt-get update && sudo apt-get install -y certbot
 sudo certbot certonly -n --standalone --email szczepanik.antoni@gmail.com --agree-tos --domains caddy.antoniszczepanik.com
 sudo caddy start
